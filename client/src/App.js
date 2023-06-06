400eaa25308c972ec12a1a7652cfdf44495cbc36
hbapte <ijbapte@gmail.com>
2023-06-07 00:45:26 +0200

    components styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
-import './index.css'
+import React from "react";
+import "./index.css";
 
+// Components
+import Hero from "./components/Hero";
+import Insurance from "./components/Insurance";
+import Services from "./components/Services";
+import Team from "./components/Team";
+import Testimonials from "./components/Testimonials";
+import FAQs from "./components/FAQs";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
-export default function App(){
-    return (
-        <div>
-        <Footer />
-        </div>
-        
-    )
+export default function App() {
+  return (
+    <div>
+      <Hero />
+      <Services />
+      <Insurance />
+      <Team />
+      <Testimonials />
+      <FAQs />
+      <Contact />
+      <Footer />
+    </div>
+  );
 }
