fab2f948d7425f032655fa0e5df3c311a0a94ea6
hbapte <ijbapte@gmail.com>
2023-06-06 14:16:27 +0200

    configuration

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",
-
-],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
             colors: {
@@ -10,7 +8,5 @@ module.exports = {
       }
     },
   },
-  plugins: [
-     
-  ],
+  plugins: [],
 };
