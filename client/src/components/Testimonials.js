ad938d033fa4040f084c2d12fae58c56174649e3
hbapte <ijbapte@gmail.com>
2023-06-13 18:55:02 +0200

    increase testimonial card

--- client/src/components/Testimonials.js
+++ client/src/components/Testimonials.js
@@ -129,8 +129,8 @@ export default function App() {
                         d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"
                       ></path>
                     </svg>
-                    I had an outstanding experience at Dentrw dental cinic. Their staff is very friendly and their facilities are state-of-the-art. 
-                    They took great care of me, the dental care was done with precision and expertise.
+                    I had an outstanding experience at Dentrw dental clinic. Their staff is very friendly and their facilities are state-of-the-art. 
+                    They took great care of me, the dental care was done with precision and expertise. Thank  you DentRw !
                   </p>
                   <ul class="flex justify-center mb-6">
                     <li>
