6510497d57b2104b3303051e9d8c7e1a0945b535
ISHIMWE Jean Baptiste <ijbapte@gmail.com>
2025-04-06 15:55:29 +0200

    feat: Add NotFound and Terms pages

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,28 @@
+import Navbar from "../components/header/Navbar";
+import Hero from "../components/Hero";
+import Insurance from "../components/Insurance";
+import Services from "../components/Services";
+import Team from "../components/Team";
+// import Testimonials from "../components/Testimonials";
+import FAQs from "../components/FAQs";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import Features from "../components/Features";
+
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Insurance />
+      <Features  /> 
+      <Team />
+      {/* <Testimonials /> */}
+      <FAQs />
+      <Contact />
+      < Footer />
+    </div>
+  );
+}
---
+++ client/src/pages/Terms.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TermsOfServices from '../components/Terms';
+
+const TermsPage: React.FC = () => {
+    return (
+        <>
+           <TermsOfServices />
+        </>
+    );
+};
+
+export default TermsPage;
