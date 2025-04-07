a318b47789ec02437ed5b83fffe5e9ff79cda7d9
ISHIMWE Jean Baptiste <ijbapte@gmail.com>
2025-04-07 11:27:30 +0200

    refactor: remove unused user repository files to streamline codebase

--- server/src/modules/user/repository/registerRepository.ts
+++
@@ -1,8 +0,0 @@
-import User from "../../../database/models/user";
-import bcrypt from "bcryptjs";
-
-export const createUser = async (names: string, email: string, username: string, password: string) => {
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = new User({ names, email, username, password: hashedPassword });
-  await newUser.save();
-};
