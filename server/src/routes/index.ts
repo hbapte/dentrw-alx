6b77ba1f825595a3ae73b715bf93427af24d8363
ISHIMWE Jean Baptiste <ijbapte@gmail.com>
2025-04-07 11:31:26 +0200

    feat: add user, patient, doctor, appointment, and medical record routes to the main router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,13 +1,23 @@
-import express from 'express';
-import authRouter from './authRoutes';
-import profileRouter from './profileRoutes';
-import MFArouter from './twoFactorAuthRoutes';
+import express from "express"
+import authRouter from "./authRoutes"
+import profileRouter from "./profileRoutes"
+import MFArouter from "./twoFactorAuthRoutes"
+import userRouter from "./userManagementRoutes"
+import patientManagementRouter from "./patientManagementRoutes"
+import doctorManagementRouter from "./doctorManagementRoutes"
+import appointmentRouter from "./appointmentRoutes"
+import medicalRecordRouter from "./medicalRecordRoutes"
 
-const router = express.Router();
+const router = express.Router()
 
-router.use('/auth', authRouter);
-router.use('/profile', profileRouter); 
-router.use('/2fa', MFArouter);
+router.use("/auth", authRouter)
+router.use("/profile", profileRouter)
+router.use("/2fa", MFArouter)
+router.use("/users", userRouter)
+router.use("/patients", patientManagementRouter)
+router.use("/doctors", doctorManagementRouter)
+router.use("/appointments", appointmentRouter)
+router.use("/medical-records", medicalRecordRouter)
 
-export default router;
+export default router
 
