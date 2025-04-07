a3c12ac6b8d52c1ec7b2d7d58aa6ec8a98c67ce8
ISHIMWE Jean Baptiste <ijbapte@gmail.com>
2025-04-07 13:49:05 +0200

    fix: add vite-plugin-vercel to package.json and configure in vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import vercel from 'vite-plugin-vercel'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    vercel()
   ],
 })
