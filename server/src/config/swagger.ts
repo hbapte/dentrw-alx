988c2fff3d5e57191964dc7444e36b23193cd996
ISHIMWE Jean Baptiste <ijbapte@gmail.com>
2025-05-04 11:00:53 +0200

    fix(swagger): update local server URL to match the correct port

--- server/src/config/swagger.ts
+++ server/src/config/swagger.ts
@@ -12,7 +12,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:5500/api',
+        url: 'http://localhost:3000/api',
         description: "local host server"
       },
       {
