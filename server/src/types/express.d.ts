186d3cd0daff90a9b94ae725c18cc7f396004a66
ISHIMWE Jean Baptiste <ijbapte@gmail.com>
2025-05-04 10:49:48 +0200

    feat: add startTime property to Express Request interface

--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -1,9 +1,14 @@
+//server\src\types\express.d.ts
 import { User } from '../database/models/user';
 
 declare global {
   namespace Express {
     interface Request {
       user?: User;
+      startTime?: number
     }
   }
 }
+
+
+export {};
