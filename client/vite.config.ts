03cddc54be21dddf688f88dbe46d8090f0cac972
ISHIMWE Jean Baptiste <ijbapte@gmail.com>
2025-05-04 11:03:59 +0200

    feat(config): add path aliasing and server configuration to Vite setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,9 @@
+// client\vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import vercel from 'vite-plugin-vercel'
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +12,18 @@ export default defineConfig({
     tailwindcss(),
     vercel()
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
 })
