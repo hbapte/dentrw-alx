b5706d65a7e43b9f560ca1f00470803baf105c3e
Ishimwe Jean Baptiste <ijbapte@gmail.com>
2026-08-28 16:56:15 +0200

    test: replace placeholder test with real AnnouncementBar specs

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,35 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+// jest-dom adds custom matchers for asserting on DOM nodes.
+// https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom"
+
+// --- jsdom polyfills for browser APIs used by third-party UI libs ---
+// (swiper, react-awesome-reveal)
+
+if (!window.matchMedia) {
+  window.matchMedia = (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  })
+}
+
+class MockObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+  takeRecords() {
+    return []
+  }
+}
+
+if (!window.IntersectionObserver) {
+  window.IntersectionObserver = MockObserver
+}
+if (!window.ResizeObserver) {
+  window.ResizeObserver = MockObserver
+}
